6f3fb8b19d8fa4b7ad614d0bb9798cb0ea1fef8a
revant <revant.jha@hashedin.com>
2017-02-10 14:47:24 +0530

    collapsible navbar completed

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,5 +1,13 @@
 $(function() {
 	$(".toggle").on("click", function() {
   		$(".toggle").parent().toggleClass('active');
+  		if($(".toggle").children().attr("class").split(' ')[1] == "fa-reorder"){
+  			$(".toggle").children().attr("class" , "fa fa-times fa-2x")
+  			$(".logo")[0].style.display = 'none';
+  		}else if($(".toggle").children().attr("class").split(' ')[1] == "fa-times"){
+  			$(".toggle").children().attr("class" , "fa fa-reorder fa-2x")
+  			$(".logo")[0].style.display = 'block';
+  		}
+
 	}); 
 });
