7dfefd9c2590c21c954d2e700ad42ecf9cdc7fe9
revant <revant.jha@hashedin.com>
2017-02-13 11:20:33 +0530

    Changes made wrt to the comments

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,12 +1,14 @@
+"use strict";
+
 $(function() {
 	$(".toggle").on("click", function() {
   		$(".toggle").parent().toggleClass('active');
-  		if($(".toggle").children().attr("class").split(' ')[1] == "fa-reorder"){
-  			$(".toggle").children().attr("class" , "fa fa-times fa-2x")
+  		if($(".toggle").children().attr("class").split(' ')[1] === "fa-reorder"){
+  			$(".toggle").children().attr("class" , "fa fa-times fa-2x");
   			$(".logo")[0].style.display = 'none';
         $("html")[0].style.overflow="hidden";
-  		}else if($(".toggle").children().attr("class").split(' ')[1] == "fa-times"){
-  			$(".toggle").children().attr("class" , "fa fa-reorder fa-2x")
+  		}else if($(".toggle").children().attr("class").split(' ')[1] === "fa-times"){
+  			$(".toggle").children().attr("class" , "fa fa-reorder fa-2x");
   			$(".logo")[0].style.display = 'block';
         $("html")[0].style.overflow="scroll";
   		}
